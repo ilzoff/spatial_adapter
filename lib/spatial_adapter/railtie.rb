5ad5255f5c249886c5cedae40fdcc7468a1b25b5
il.zoff <il.zoff@gmail.com>
2013-04-05 00:53:43 +0400

    Ability to prevent autoloading of spatial_adapter in rails

--- lib/spatial_adapter/railtie.rb
+++ lib/spatial_adapter/railtie.rb
@@ -1,11 +1,13 @@
 module SpatialAdapter
   class Railtie < Rails::Railtie
-    initializer "spatial_adapter.load_current_database_adapter" do
-      adapter = ActiveRecord::Base.configurations[Rails.env]['adapter']
-      begin
-        require "spatial_adapter/#{adapter}"
-      rescue LoadError
-        raise SpatialAdapter::NotCompatibleError.new("spatial_adapter does not currently support the #{adapter} database.")
+    initializer "spatial_adapter.load_current_database_adapter" do |app|
+      unless app.config.spatial_adapter == false
+        adapter = ActiveRecord::Base.configurations[Rails.env]['adapter']
+        begin
+          require "spatial_adapter/#{adapter}"
+        rescue LoadError
+          raise SpatialAdapter::NotCompatibleError.new("spatial_adapter does not currently support the #{adapter} database.")
+        end
       end
     end
   end
